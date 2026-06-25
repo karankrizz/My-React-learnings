1b37da13654bc5b75ffa35b673536a65f12d78ed
karankrizz <kkriz448@gmail.com>
2026-06-25 21:39:09 +0530

    workout: shared the style from the parent to child through props

--- src/pages/CustomerList.jsx
+++ src/pages/CustomerList.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-export default function CustomerList(props){
-    return <div>
-        <p>Hi this is {props?.customername}</p>
+export default function CustomerList(props) {
+  return (
+    <div>
+      <p>
+        Hi this is <strong style={props?.nameStyle}>{props?.customername} </strong>
+      </p>
+      <p>
+        <strong>Age :</strong> {props?.customerAge}{" "}
+      </p>
+      <p>
+        <strong>Qualification :</strong> {props?.customerQualification}{" "}
+      </p>
     </div>
+  );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import CustomerList from "./CustomerList";
 export default function HomePage() {
   const customerName = "Kirubhakaran R";
+  const customerAge = 25;
+  const customerQualification = "Msc Mathematics";
+  const nameStyle={
+    color:"red",
+  }
 
   return (
     <div>
-      <h1> Hi this is the Homepage</h1>
-      <CustomerList customername={customerName} />
+      <h1 > Hi this is the Homepage</h1>
+      <CustomerList
+        customername={customerName.toUpperCase()}
+        customerAge={customerAge}
+        customerQualification={customerQualification}
+        nameStyle={nameStyle}
+      />
     </div>
   );
 }
