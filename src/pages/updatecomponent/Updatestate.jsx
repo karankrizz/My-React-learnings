dafa8466a7d843608787570fb018ef74f4dbc395
karankrizz <kkriz448@gmail.com>
2026-06-26 00:02:59 +0530

    worked out : multiple at the same time using the set operation in the use state

--- src/pages/updatecomponent/Updatestate.jsx
+++ src/pages/updatecomponent/Updatestate.jsx
@@ -1,17 +1,25 @@
-import React,{useState} from "react";
-export default function Updatestate(){
-    const [name,setname]=useState("kirubhakaran")
-    const[age,setage]=useState(25)
-    const changeName=()=>{
-        setname("Vigneshwaran")
-    }
-    const changeAge=()=>{
-        setage(31)
-    }
-    return <div>
-        <p>Student Name : {name}</p>
-        <p>Student Age :{age}</p>
-        <button onClick={changeName}>Change Name</button>
-        <button onClick={changeAge}>Change Age</button>
+import React, { useState } from "react";
+export default function Updatestate() {
+  const [name, setname] = useState("kirubhakaran");
+  const [age, setage] = useState(25);
+  const changeName = () => {
+    setname("Vigneshwaran");
+  };
+  const changeAge = () => {
+    setage(31);
+  };
+  const changeTotaly=()=>{
+    setname("Snow")
+    setage(4)
+  }
+  return (
+    <div>
+      <p>Student Name : {name}</p>
+      <p>Student Age :{age}</p>
+      <button onClick={changeName}>Change Name</button>
+      <button onClick={changeAge}>Change Age</button>
+      <button onClick={changeTotaly}>Change totally</button>
+
     </div>
+  );
 }
