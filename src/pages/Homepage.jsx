216cdaf449cb1a031f57d797fbb8435163bff940
karankrizz <kkriz448@gmail.com>
2026-06-25 15:07:22 +0530

    worked out  on the  onClick

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 export default function Homepage() {
-  const details = ["kirubhakaran", "25", "Msc mathematics","R"];
-  const [name, age, degree,initial] = details;
+  const details = ["kirubhakaran", "25", "Msc mathematics", "R"];
+  const [name, age, degree, initial] = details;
+  const skills = ["html", "css", "bootstrap", "javascript", "React"];
   const workingStatus = false;
   const headingStyle = {
     color: "red",
@@ -13,22 +14,39 @@ export default function Homepage() {
   const yesStyle = {
     color: "green",
   };
+  const status = () => {
+    console.log("Details submitted ");
+  };
   return (
     <div>
       <h1 style={headingStyle}>About Me</h1>
 
-      <p><strong>NAME : </strong>{name.toUpperCase()+' '+initial}</p>
-      <p><strong>AGE : </strong>{age}</p>
-      <p><strong>DEGREE : </strong>{degree.toUpperCase()}</p>
+      <p>
+        <strong>NAME : </strong>
+        {name.toUpperCase() + " " + initial}
+      </p>
+      <p>
+        <strong>AGE : </strong>
+        {age}
+      </p>
+      <p>
+        <strong>DEGREE : </strong>
+        {degree.toUpperCase()}
+      </p>
+      <div>
+        Skills:{skills.map((value,index)=><p key={index}>{value}</p>) }
+ 
+      </div>
 
       <p>
-       <strong> Currently working:{" "}</strong>
+        <strong> Currently working: </strong>
         {workingStatus ? (
           <strong style={yesStyle}>Yes</strong>
         ) : (
           <strong style={noStyle}>NO</strong>
         )}
       </p>
+      <button onClick={status}>Submit</button>
     </div>
   );
 }
