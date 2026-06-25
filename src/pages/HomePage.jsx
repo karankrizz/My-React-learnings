ea4f40367e9891331cd9210f1b686598308e3f04
karankrizz <kkriz448@gmail.com>
2026-06-25 16:51:24 +0530

    workout: worked on the class components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,39 @@
-export default function HomePage(){
-    return <div>
-        <h1>kirubha </h1>
-    </div>
+import React from "react";
+export default class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      canditateFirstName: "kirubha",
+      canditateAge: 25,
+      degree: "Msc mathematics",
+      skills: ["html", "css", "javascript", "bootstrap"],
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <p>
+          <strong>Name :</strong> {this.state.canditateFirstName}
+        </p>
+        <p>
+          <strong>Age :</strong> {this.state.canditateAge}
+        </p>
+        <p>
+          <strong>Degree :</strong> {this.state.degree}
+        </p>
+        <p>
+          <strong>Skills :</strong>{" "}
+          {this.state.skills[0] +
+            " , " +
+            this.state.skills[1] +
+            " , " +
+            this.state.skills[2] +
+            " , " +
+            this.state.skills[3] +
+            "."}
+        </p>
+      </div>
+    );
+  }
 }
