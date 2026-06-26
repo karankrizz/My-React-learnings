83432b9ea7590e9dbe3053e68807e335b598e2fb
karankrizz <kkriz448@gmail.com>
2026-06-26 12:49:13 +0530

    workout:basic form

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,72 @@
-export default function Homepage() {
-  const details = ["kirubha", "25", "Msc mathematics"];
-  const workingStatus = false;
-  const headingStyle = {
-    color: "red",
-    backgroundColor: "black",
-    width: 150,
-  };
-  const noStyle = {
-    color: "red",
-  };
-  const yesStyle = {
-    color: "green",
-  };
-  return (
-    <div>
-      <h1 style={headingStyle}>About Me</h1>
+ import React,{useState} from "react";
+ export default function Homepage(){
+  const starStyle={
+    color:"red"
+  }
+  const handleSubmit=(event)=>{
+    event.preventDefault()
+    console.log("Form is submitted")
 
-      {details.map((value, index) => (
-        <p key={index}>
-          <strong> {value}</strong>
-        </p>
-      ))}
-      
-      <p>
-        Currently working:{" "}
-        {workingStatus ? (
-          <strong style={yesStyle}>Yes</strong>
-        ) : (
-          <strong style={noStyle}>NO</strong>
-        )}
-      </p>
-    </div>
-  );
-}
+  }
+
+return <div>
+
+  <h1>Form Registration</h1>
+  <div>
+    <form onSubmit={handleSubmit}>
+      <div>
+
+        <label htmlFor="username"> UserName <span style={starStyle}>*</span></label>
+        <input type="text"
+        name="username"
+        id="username" />
+      </div>
+            <div>
+
+        <label htmlFor="password"> Password <span style={starStyle}>*</span> </label>
+        <input type="password"
+        name="password"
+        id="password" />
+      </div>
+  <div>
+    <button
+    type="submit">
+      Submit
+    </button>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </form>
+  </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+ }
