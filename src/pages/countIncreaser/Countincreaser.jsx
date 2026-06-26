5846b6364ef8afa2b09d01acc2b0e5a3aab0e3d8
karankrizz <kkriz448@gmail.com>
2026-06-26 12:37:21 +0530

    .

--- src/pages/countIncreaser/Countincreaser.jsx
+++ src/pages/countIncreaser/Countincreaser.jsx
@@ -1,14 +1,37 @@
-import React,{useState} from "react";
-export default function Countincreaser(){
-    const [count,setcount]=useState(0)
-    const [fruits,setfruits]=useState(["apple","orange"])
-    return <div>
-        <p>COUNT : {count}  </p>
-        <button onClick={()=>setcount(count+1)}>ADD COUNT</button>
-        <div>
-            <p>fruits : </p>
-            {fruits.map((value,index)=><p>{value}</p>)}
-            <button onClick={()=>setfruits([...fruits,"mango"])}>ADD FRUITS</button>
-        </div>
+import React, { useState } from "react";
+export default function Countincreaser() {
+  const [count, setcount] = useState(0);
+  const [fruits, setfruits] = useState(["apple", "orange"]);
+  const [student, setstudent] = useState({
+    name: "kirubhakaran",
+    age: 25,
+    degree: "Bsc Mathematics ",
+  });
+  return (
+    <div>
+      <p>COUNT : {count} </p>
+      <button onClick={() => setcount(count + 1)}>ADD COUNT</button>
+      <hr />
+      <div>
+        <p>fruits : </p>
+        {fruits.map((value, index) => (
+          <p>{value}</p>
+        ))}
+        <button onClick={() => setfruits([...fruits, "mango"])}>
+          ADD FRUITS
+        </button>
+      </div>
+      <hr />
+      <div>
+        <p>NAME :{student?.name} </p>
+        <p>AGE : {student?.age} </p>
+        <p>DEGREE : {student?.degree} </p>
+        <button
+          onClick={() => setstudent({ ...student, degree: "Msc Mathematics" })}
+        >
+          update the degree
+        </button>
+      </div>
     </div>
+  );
 }
